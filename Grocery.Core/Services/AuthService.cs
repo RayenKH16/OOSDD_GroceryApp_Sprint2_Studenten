1df6a2516571aee28ed085cc9b0e3a0f6962d76a
Rayen87 <119052841+Rayen87@users.noreply.github.com>
2025-09-19 21:27:39 +0200

    Usecase6
    Toegevoegd/verbeterd voor volledige login-functionaliteit: Client-properties, ClientRepository zoeklogica, AuthService loginvalidatie, LoginViewModel bindingen en LoginView UI.

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -7,15 +7,28 @@ namespace Grocery.Core.Services
     public class AuthService : IAuthService
     {
         private readonly IClientService _clientService;
+
         public AuthService(IClientService clientService)
         {
             _clientService = clientService;
         }
+
         public Client? Login(string email, string password)
         {
-            //Vraag de klantgegevens [Client] op die je zoekt met het opgegeven emailadres
-            //Als je een klant gevonden hebt controleer dan of het password matcht --> PasswordHelper.VerifyPassword(password, passwordFromClient)
-            //Als alles klopt dan klantgegveens teruggeven, anders null
+            // Zoek de client op basis van email
+            var client = _clientService.Get(email);
+
+            if (client == null)
+                return null;
+
+            // Controleer het wachtwoord
+            bool isValid = PasswordHelper.VerifyPassword(password, client.Password);
+
+            if (isValid)
+            {
+                return client;
+            }
+
             return null;
         }
     }
